b134c0089acd6c8c2bf38ba4284da7b0ffff38d8
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2024-04-19 16:37:23 -0500

    more changes to work on macOS

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -9,7 +9,7 @@ alias back='cd -'
 alias cls=clear
 alias cp='cp -iv'
 alias cpr='cp -r'
-alias ez='exec zsh'
+alias ezsh='exec zsh'
 alias grep='grep --color=auto'
 alias his=history
 alias jctl=journalctl
@@ -22,7 +22,6 @@ alias path='sed "s/:/\n/g" <<< "$PATH"'
 alias rm='rm -I'
 alias rmdir='rmdir -v'
 alias rmr='rm -r'
-alias trash='gio trash'
 alias uuid='uuidgen --random'
 
 ## cd
@@ -32,15 +31,15 @@ alias .....='cd ../../../..'
 alias ......='cd ../../../../..'
 
 ## ls
-# replace ls with exa
-alias ls='exa --git --icons --group-directories-first -F'
-alias tree='exa --tree'
+# replace ls with eza
+alias ls='eza --git --icons --group-directories-first -F'
+alias tree='eza --tree'
 alias la='ls -a'
 alias ll='ls -l'
 alias lal='ls -al'
 alias lla='ls -la'
-alias llt='exa -l -t modified --sort newest'
-alias llat='exa -al -t modified --sort newest'
+alias llt='eza -l -t modified --sort newest'
+alias llat='eza -al -t modified --sort newest'
 
 ### external tools
 alias bat=batcat
@@ -59,13 +58,9 @@ alias fd='fd -H'
 alias gob='go build .'
 # run the go project in the cwd
 alias gor='go run .'
-alias info='pinfo'
 alias jl=jless
 # colorize json
 alias jq='jq -C'
-alias kc=kubectl
-alias m=micro
-alias md=mdcat
 # run local version of ts-node
 alias nts='npx ts-node'
 alias py=python3
@@ -75,8 +70,6 @@ alias python=python3
 alias rgf='rg -F'
 # case-insensitive search
 alias rgi='rg -i'
-# keep making this typo for some reason
-alias rust='echo -e "\nthink you mean _rusH_"'
 alias sesh='tmux new-session -A -s sesh'
 alias v=nvim
 alias vi=nvim
@@ -105,27 +98,3 @@ alias nrl='npm run lint'
 alias nrestart='npm run build && npm run start'
 alias nrs='npm run start'
 alias nrt='npm run test'
-
-## zsh-z
-alias za='z --add' # add directory to db
-alias zc='z -c'    # only match subdirs of pwd
-alias ze='z -e'    # echo best match without jumping
-alias zl='z -l'    # list matches without jumping
-alias zr='z -r'    # match by rank
-alias zs='z -s'    # only match subdirs of pwd
-alias zt='z -t'    # match by time
-alias zt='z -x'    # remove directory from db (defaults to pwd)
-alias zxr='x -xR'  # remove dir and all subdirs from db (defaults to pwd)
-
-alias aptup='sudo apt update && sudo apt upgrade --yes'
-alias audio-reload='systemctl --user restart pipewire pipewire-pulse'
-alias guake-export="guake --save-preferences ~/.dotfiles/app-exports/guake-preferences"
-alias kmonad-reload="pkill kmonad && nohup kmonad $HOME/.config/kmonad/dell_latitude.kbd &"
-alias snap-store-refresh='pkill snap-store && sudo snap refresh snap-store'
-alias update='sudo apt update && sudo apt upgrade --yes && brew upgrade && flatpak update'
-
-# search Spotify playlists
-alias sm="$HOME/bin/playlist-search.sh"
-alias sp="$HOME/bin/playlist-search.py"
-alias spd="sp --sort date"
-alias spalb="sp --type album"
--- dot_config/zsh/tools.zsh
+++ dot_config/zsh/tools.zsh
@@ -1,60 +1,29 @@
 #!/usr/bin/env zsh
 # External Zsh tools
 
-# Put homebrew first because some tools are in the homebrew path
+# IMPORTANT! Put homebrew first because some tools are in the homebrew path
 
-## homebrew
-export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
-## GPG (GNU Privacy Guard or GnuPG)
-export GPG_TTY=$(tty)
-
-## zsh-completions
-
+# zsh-completions
 fpath=(/usr/local/share/zsh-completions $fpath) # activate zsh-completions
-fpath=($HOME/.zsh/zsh-completions $fpath)       # activate zsh-completions
+fpath=($HOME/.zsh/zsh-completions $fpath)       # activate zsh-completions## homebrew
 
-## kitty
-export PATH=$HOME/.local/kitty.app/bin:$PATH
-# convenience function
-function update-kitty() {
-    curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-}
+# homebrew
+set PATH /opt/homebrew/bin $PATH
+eval "$(/opt/homebrew/bin/brew shellenv)" # homebrew
 
-## broot
-source $HOME/.config/broot/launcher/bash/br
-
-## zsh-autosuggestions
-source /home/linuxbrew/.linuxbrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=213"
-
-## zsh-syntax-highlighting
-# dracula theme; must come before sourcing the main package
-source $HOME/tools/dracula-zsh-syntax-highlighting/zsh-syntax-highlighting.sh
-source /home/linuxbrew/.linuxbrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-export ZSH_HIGHLIGHT_HIGHLIGHTERS_DIR=/home/linuxbrew/.linuxbrew/share/zsh-syntax-highlighting/highlighters
-
-## starship
+# starship
 eval "$(starship init zsh)"
 
-# set window title via starship
+#: set window title via starship
 function set_win_title() {
     echo -ne "\033]0; $(realpath .) \007"
 }
 
 precmd_functions+=(set_win_title)
 
-## nodenv
-export PATH=$HOME/.nodenv/bin:$PATH
-eval "$(nodenv init -)"
+# nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh" # This loads nvm
 
-## pnpm
-export PNPM_HOME="$HOME/.local/share/pnpm"
-export PATH="$PNPM_HOME:$PATH"
-
-## set Ripgrep configuration file
+# ripgrep configuration file
 export RIPGREP_CONFIG_PATH="$HOME/.config/ripgreprc"
-
-## https://github.com/agkozak/zsh-z
-#source "$HOME"/tools/.zsh-z/zsh-z.plugin.zsh
