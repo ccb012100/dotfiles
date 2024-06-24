0c889e72f4f84a34db7b3dc97564949f388a1adf
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2024-06-24 16:09:53 -0500

    switch from nvm to fnm

--- dot_config/bash/tools.sh
+++ dot_config/bash/tools.sh
@@ -6,10 +6,6 @@
 export PATH="/opt/homebrew/bin:$HOME/bin:$HOME/.local/bin:$PATH"
 eval "$(/opt/homebrew/bin/brew shellenv)" # homebrew
 
-# NVM
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-
 # starship
 eval "$(starship init bash)"
 
