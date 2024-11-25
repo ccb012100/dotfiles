70ff79de88e75f5d0e9adf6bf35738f6c1b15dd8
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2024-11-25 15:23:48 -0600

    hammerspoon: bindings for vol up/down

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -11,3 +11,26 @@ hs.alert.defaultStyle.textColor = { hex = "#ffcc2f", alpha = 1 } -- alert text =
 hs.alert.defaultStyle.textFont = "IBM Plex Sans"
 hs.alert.defaultStyle.textSize = 24
 hs.alert.defaultStyle.strokeColor = { hex = "#ffcc2f", alpha = 1 } -- alert outline = amber
+
+-- bind Cmd+Opt+<mouse wheel scroll up/down> to Volume up/down
+local eventtap = require("hs.eventtap")
+local watcher = eventtap.new({ eventtap.event.types.scrollWheel }, function(event)
+    local mods = hs.eventtap.checkKeyboardModifiers()
+
+    -- only if mods = Cmd+Opt
+    if not mods.cmd or not mods.alt or mods.ctrl or mods.fn or mods.shift then
+        return false -- false = propogate event
+    end
+
+    local direction = event:getProperty(hs.eventtap.event.properties.scrollWheelEventFixedPtDeltaAxis1)
+
+    if direction > 0 then
+        hs.eventtap.event.newSystemKeyEvent("SOUND_UP", true):post()
+        hs.eventtap.event.newSystemKeyEvent("SOUND_UP", false):post()
+    elseif direction < 0 then
+        hs.eventtap.event.newSystemKeyEvent("SOUND_DOWN", true):post()
+        hs.eventtap.event.newSystemKeyEvent("SOUND_DOWN", false):post()
+    end
+
+    return true -- true = delete event
+end):start()
