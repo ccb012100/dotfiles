e8dbc7e28e7209fefd8002f4c304d6e8a33dff4a
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2025-08-20 16:06:31 -0500

    wezterm: add theme switcher; modify shortcuts

---
+++ dot_config/wezterm/helpers.lua
@@ -0,0 +1,39 @@
+local wezterm = require 'wezterm'
+local act = wezterm.action
+
+local module = {}
+
+function module.theme_switcher(window, pane)
+    -- get builtin color schemes
+    local schemes = wezterm.get_builtin_color_schemes()
+    local choices = {}
+
+    -- populate theme names in choices list
+    for key, _ in pairs(schemes) do
+        table.insert(choices, { label = tostring(key) })
+    end
+
+    -- sort choices list
+    table.sort(choices, function(c1, c2)
+        return c1.label < c2.label
+    end)
+
+    window:perform_action(
+        act.InputSelector {
+            title = '🎨 Pick a Theme!',
+            choices = choices,
+            fuzzy = true,
+
+            action = wezterm.action_callback(function(window, _, id, label)
+                if not id and not label then
+                    wezterm.log_info 'cancelled'
+                else
+                    window:set_config_overrides { color_scheme = label }
+                end
+            end),
+        },
+        pane
+    )
+end
+
+return module
