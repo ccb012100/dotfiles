82d5ee3f4cb41aecff6f9e08b003a9d0263eb327
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2025-11-24 14:05:03 -0600

    fish: don't need to get confirmation in the playlist update script

--- dot_config/fish/functions/playlist_sync.fish
+++ dot_config/fish/functions/playlist_sync.fish
@@ -23,42 +23,42 @@ function playlist_sync --description 'Sync Spotify playlists to local DB/spreads
         return 1
     end
 
-    while read --nchars 1 -l response --prompt-str="❔ Sync ccb012100/spotify-playlists album tsv files? (y/n) "
-        switch $response
-            case y Y
-                set_color purple
-                echo \nSyncing tsv files...
-                set_color normal
+    # while read --nchars 1 -l response --prompt-str="❔ Sync ccb012100/spotify-playlists album tsv files? (y/n) "
+    #     switch $response
+    #         case y Y
+    set_color purple
+    echo \nSyncing tsv files...
+    set_color normal
 
-                $playlister_script sync tsv
+    $playlister_script sync tsv
 
-                if test $status -ne 0
-                    set_color brred
-                    echo \n\t --- Playlist TSV sync failed! ---\n
-                    set_color normal
+    if test $status -ne 0
+        set_color brred
+        echo \n\t --- Playlist TSV sync failed! ---\n
+        set_color normal
 
-                    return 1
-                end
-
-                # set_color purple
-                # echo \nSynced tsv files.\n
-                # set_color normal
-
-                break
-            case n N
-                set_color purple
-                echo \nNot syncing album tsv files.\n
-                set_color normal
-
-                break
-            case '*'
-                set_color bryellow
-                echo Invalid choice: $response
-                set_color normal
-
-                continue
-        end
+        return 1
     end
+
+    #             # set_color purple
+    #             # echo \nSynced tsv files.\n
+    #             # set_color normal
+
+    #             break
+    #         case n N
+    #             set_color purple
+    #             echo \nNot syncing album tsv files.\n
+    #             set_color normal
+
+    #             break
+    #         case '*'
+    #             set_color bryellow
+    #             echo Invalid choice: $response
+    #             set_color normal
+
+    #             continue
+    #     end
+    # end
 
     # set_color brgreen
     # echo \n\t--- Completed successfully! ---
