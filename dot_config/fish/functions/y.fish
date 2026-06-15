666660e476be2d919f363e617170b0189eaff0ca
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2026-06-15 17:14:43 -0500

    fish: add yazi function; add dntq abbr

--- dot_config/fish/functions/y.fish
+++ dot_config/fish/functions/y.fish
@@ -1,4 +1,4 @@
-function f --description "jump to directory with zoxide on 'q' ('Q' to exit without jumping)"
+function y --description "open yazi and jump to directory with zoxide on 'q' ('Q' to exit without jumping)"
     set tmp (mktemp -t "yazi-cwd.XXXXXX")
     yazi $argv --cwd-file="$tmp"
     if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
--- dot_config/fish/functions/z.fish
+++
@@ -1,4 +0,0 @@
-function z --description 'Stop using z for zoxide'
-    echo use j for zoxide
-    return 1
-end
