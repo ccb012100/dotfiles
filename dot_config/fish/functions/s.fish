081bdfffb2c9f5cbd91c65072647d3d703ff3e1e
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2026-07-28 15:06:42 -0500

    fish: remove unused variables in function

--- dot_config/fish/functions/s.fish
+++ dot_config/fish/functions/s.fish
@@ -1,6 +1,3 @@
 function s --description 'Find matches in starred albums for search text'
-    set pldb $HOME/playlister.sqlite3
-    set playlister_script $HOME/bin/playlister.sh
-
     rg "$argv" $HOME/bin/albums/sorted_albums.tsv | column -t -s \t -c1
 end
